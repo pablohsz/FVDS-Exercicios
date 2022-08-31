adef60d513ba29c9cf9f759af23fa0bef8309977
pablohsz <pablosouza.dev@gmail.com>
2022-08-31 14:13:38 -0300

    alterações na árvore dos repositórios

---
+++ FormsExercicio/FormsExercicio/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace FormsExercicio
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbValordoProduto = new System.Windows.Forms.Label();
+            this.lbFrete = new System.Windows.Forms.Label();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbProdutos = new System.Windows.Forms.ComboBox();
+            this.cmbLocal = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CALCULAR VALOR TOTAL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SAIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lbTotal);
+            this.groupBox1.Controls.Add(this.lbFrete);
+            this.groupBox1.Controls.Add(this.lbValordoProduto);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(507, 188);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações de Valores:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbLocal);
+            this.groupBox2.Controls.Add(this.cmbProdutos);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(507, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Selecione o que deseja:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor do Produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Frete";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total";
+            // 
+            // lbValordoProduto
+            // 
+            this.lbValordoProduto.AutoSize = true;
+            this.lbValordoProduto.Location = new System.Drawing.Point(244, 38);
+            this.lbValordoProduto.Name = "lbValordoProduto";
+            this.lbValordoProduto.Size = new System.Drawing.Size(13, 13);
+            this.lbValordoProduto.TabIndex = 3;
+            this.lbValordoProduto.Text = "0";
+            // 
+            // lbFrete
+            // 
+            this.lbFrete.AutoSize = true;
+            this.lbFrete.Location = new System.Drawing.Point(244, 93);
+            this.lbFrete.Name = "lbFrete";
+            this.lbFrete.Size = new System.Drawing.Size(13, 13);
+            this.lbFrete.TabIndex = 4;
+            this.lbFrete.Text = "0";
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(244, 151);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(13, 13);
+            this.lbTotal.TabIndex = 5;
+            this.lbTotal.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "R$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(215, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "R$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "R$";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Produto";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Local de Entrega";
+            // 
+            // cmbProdutos
+            // 
+            this.cmbProdutos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProdutos.FormattingEnabled = true;
+            this.cmbProdutos.Items.AddRange(new object[] {
+            "[ SELECIONE ]",
+            "Fogão",
+            "Freezer",
+            "Geladeira",
+            "Mesa",
+            "Microondas"});
+            this.cmbProdutos.Location = new System.Drawing.Point(141, 28);
+            this.cmbProdutos.Name = "cmbProdutos";
+            this.cmbProdutos.Size = new System.Drawing.Size(121, 21);
+            this.cmbProdutos.Sorted = true;
+            this.cmbProdutos.TabIndex = 2;
+            this.cmbProdutos.SelectedIndexChanged += new System.EventHandler(this.cmbProdutos_SelectedIndexChanged);
+            // 
+            // cmbLocal
+            // 
+            this.cmbLocal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLocal.FormattingEnabled = true;
+            this.cmbLocal.Items.AddRange(new object[] {
+            "[ SELECIONE ]",
+            "Anápolis",
+            "Caldas Novas",
+            "Faina",
+            "Goiânia",
+            "Goianira "});
+            this.cmbLocal.Location = new System.Drawing.Point(141, 68);
+            this.cmbLocal.Name = "cmbLocal";
+            this.cmbLocal.Size = new System.Drawing.Size(121, 21);
+            this.cmbLocal.Sorted = true;
+            this.cmbLocal.TabIndex = 3;
+            this.cmbLocal.SelectedIndexChanged += new System.EventHandler(this.cmbLocal_SelectedIndexChanged);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(531, 441);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FrmPrincipal";
+            this.Text = "Calcula Frete";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.Label lbFrete;
+        private System.Windows.Forms.Label lbValordoProduto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbLocal;
+        private System.Windows.Forms.ComboBox cmbProdutos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ FormsExercicio/FormsExercicio/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsExercicio
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmbProdutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbProdutos.SelectedIndex)
+            {
+                case 1:
+                    lbValordoProduto.Text = "500,00";
+                    break;
+
+                case 2:
+                    lbValordoProduto.Text = "2.200,00";
+                    break;
+
+                case 3:
+                    lbValordoProduto.Text = "4.000,00";
+                    break;
+
+                case 4:
+                    lbValordoProduto.Text = "800,00";
+                    break;
+
+                case 5:
+                    lbValordoProduto.Text = "1.000,00";
+                    break;
+
+            }
+        }
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            cmbProdutos.Text = "[ SELECIONE ]";
+            cmbLocal.Text = "[ SELECIONE ]";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cmbProdutos.Text = "[ SELECIONE ]";
+            cmbLocal.Text = "[ SELECIONE ]";
+            lbFrete.Text = "0";
+            lbValordoProduto.Text = "0";
+            lbTotal.Text = "0";
+        }
+
+        private void cmbLocal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbLocal.SelectedIndex)
+            {
+                case 1:
+                    lbFrete.Text = "50,00";
+                    break;
+
+                case 2:
+                    lbFrete.Text = "20,00";
+                    break;
+
+                case 3:
+                    lbFrete.Text= "40,00";
+                    break;
+
+                case 4:
+                    lbFrete.Text= "80,00";
+                    break;
+
+                case 5:
+                    lbFrete.Text = "10,00";
+                    break;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            if (cmbProdutos.SelectedIndex.Equals(0))
+            {
+                result = MessageBox.Show("Selecione um produto para continuar.", "Selecione um produto", MessageBoxButtons.OK);
+            } 
+            else if (cmbLocal.SelectedIndex.Equals(0))
+            {
+                result = MessageBox.Show("Selecione um o local de entrega para continuar.", "Selecione um local", MessageBoxButtons.OK);
+            } else
+            {
+                double total;
+                total = Convert.ToDouble(lbValordoProduto.Text) + Convert.ToDouble(lbFrete.Text);
+                lbTotal.Text = String.Format("{0:C}", total);
+            }
+        }
+    }
+}
